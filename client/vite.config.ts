20047187cb3afab40e574aa694500fe8174935a9
Kevin BIOJOUT <biojoutk1106@gmail.com>
2023-12-22 01:24:21 +0100

    refactor: client preview runs on port 4200

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  preview: {
+    port: 4200
+  },
   server: {
     proxy: {
       "/api": {
